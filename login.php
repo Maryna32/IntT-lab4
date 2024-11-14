8572f08195301d552a1229f073514e5d9d0a1974
Maryna32 <marinaburda053@gmail.com>
2024-11-14 13:55:09 +0200

    full lab

---
+++ login.php
@@ -0,0 +1,31 @@
+<?php
+$username = $_POST['username'] ?? '';
+$password = $_POST['password'] ?? '';
+
+$dataFile = 'data.txt';
+
+if (!file_exists($dataFile)) {
+    die("Файл з обліковими даними не знайдено.");
+}
+
+$line = file($dataFile, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+
+$isLoggedIn = false;
+
+foreach ($line as $line) {
+    list($validUsername, $validPassword) = explode(":", trim($line));
+
+    if ($username === $validUsername && $password === $validPassword) {
+        $isLoggedIn = true;
+        break;
+    }
+}
+if ($isLoggedIn) {
+    echo "Ви залогінені!";
+} 
+else {
+    echo "Неправильний логін або пароль.";
+}
+echo "<br><a href='index.html'>Повернутись назад</a>";
+
+?>
